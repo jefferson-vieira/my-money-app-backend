1dbbb18bbfb3d889c1709d2be72a5688970586d1
Victor Ayres <Victor.Ayres0@gmail.com>
2019-04-19 12:06:10 -0300

    SGF-11 Correções: removação do Import * do Oauth

--- src/main/java/com/mymoneyapp/backend/controller/OAuthController.java
+++ src/main/java/com/mymoneyapp/backend/controller/OAuthController.java
@@ -9,7 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -21,7 +26,7 @@ public class OAuthController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/me")
+    @GetMapping ("/me")
     @ApiOperation(value = "Retorna o usuário logado", authorizations = @Authorization("OAuth"))
     public User principal(@AuthenticationPrincipal final User user) {
         return user;
